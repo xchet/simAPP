0963b2fb86fb8c08287029eac96bbe1cbdfbdd5f
xchet <chetam.okafor@gmail.com>
2019-09-02 16:53:25 +0100

    first commit

---
+++ SimApp/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using SimApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewAll()
+        {
+            return View(GetAllEmployee());
+        }
+
+        IEnumerable<Employee> GetAllEmployee()
+        {
+            using (SimAppEntities db = new SimAppEntities())
+            {
+                return db.Employees.ToList<Employee>();
+            }
+        }
+    }
+}
